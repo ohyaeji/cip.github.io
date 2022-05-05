65bbce54161867da258b075551530211077cbd02
ohyaeji <dh0374@naver.com>
2022-05-06 03:05:20 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,15 +37,15 @@ for(i=0;i<linkCollapse.length;i++) {
     });
 }
 
-var infoTitle = document.querySelectorAll('.overlay');
-infoTitle.forEach(function(e) {
-    var w = e.offsetWidth + 10;
-    var ml = w/2;
-    e.parentElement.style.top = "82px";
-    e.parentElement.style.left = "50%";
-    e.parentElement.style.marginLeft = -ml+"px";
-    e.parentElement.style.width = w+"px";
-    e.parentElement.previousSibling.style.display = "none";
-    e.parentElement.parentElement.style.border = "0px";
-    e.parentElement.parentElement.style.background = "unset";
-});
+// var infoTitle = document.querySelectorAll('.overlay');
+// infoTitle.forEach(function(e) {
+//     var w = e.offsetWidth + 10;
+//     var ml = w/2;
+//     e.parentElement.style.top = "82px";
+//     e.parentElement.style.left = "50%";
+//     e.parentElement.style.marginLeft = -ml+"px";
+//     e.parentElement.style.width = w+"px";
+//     e.parentElement.previousSibling.style.display = "none";
+//     e.parentElement.parentElement.style.border = "0px";
+//     e.parentElement.parentElement.style.background = "unset";
+// });
