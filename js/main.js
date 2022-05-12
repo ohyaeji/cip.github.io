2303c041e577468230db90ba99c63874c8bf19a2
ohyaeji <dh0374@naver.com>
2022-05-13 04:57:15 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // /* EXPANDER MENU */
-// const showMenu = (toggleId, navbarId, bodyId) => {
-//     const toggle = document.getElementById(toggleId),
-//     navbar = document.getElementById(navbarId),
-//     bodypadding = document.getElementById(bodyId)
+const showMenu = (toggleId, navbarId, bodyId) => {
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
 
-//     if( toggle && navbar ) {
-//         toggle.addEventListener('click', ()=>{
-//             navbar.classList.toggle('expander');
+    if( toggle && navbar ) {
+        toggle.addEventListener('click', ()=>{
+            navbar.classList.toggle('expander');
 
-//             bodypadding.classList.toggle('body-pd');
-//         })
-//     }
-// }
+            bodypadding.classList.toggle('body-pd');
+        })
+    }
+}
 
-// showMenu('nav-toggle', 'navbar', 'body-pd')
+showMenu('nav-toggle', 'navbar', 'body-pd')
 
 // // Expander info
 // const showInfo = (toggleId, infoId) => {
