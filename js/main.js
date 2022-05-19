77378c397530796bd3388bd1fce093eb6f19b6fb
ohyaeji <dh0374@naver.com>
2022-05-19 18:21:43 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,15 +49,18 @@ for(i=0;i<linkCollapse.length;i++) {
     });
 }
 
-// var infoTitle = document.querySelectorAll('.overlay');
-// infoTitle.forEach(function(e) {
-//     var w = e.offsetWidth + 10;
-//     var ml = w/2;
-//     e.parentElement.style.top = "82px";
-//     e.parentElement.style.left = "50%";
-//     e.parentElement.style.marginLeft = -ml+"px";
-//     e.parentElement.style.width = w+"px";
-//     e.parentElement.previousSibling.style.display = "none";
-//     e.parentElement.parentElement.style.border = "0px";
-//     e.parentElement.parentElement.style.background = "unset";
-// });
+$(function(){
+    $("#confirm").click(function(){
+        modalClose();
+        //컨펌 이벤트 처리
+    });
+    $("#modal-open").click(function(){        
+        $("#popup").css('display','flex').hide().fadeIn();
+    });
+    $("#close").click(function(){
+        modalClose();
+    });
+    function modalClose(){
+      $("#popup").fadeOut();
+    }
+  });
